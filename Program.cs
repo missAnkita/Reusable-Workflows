0e8ac016b78d847f353e7776face0cf8779d3e9c
missAnkita <47353478+missAnkita@users.noreply.github.com>
2024-07-11 12:32:26 +0530

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace MyDotNetApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
